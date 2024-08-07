ce77254a49042494bd6e5f304627a77f87f1f4e8
Dhruv7927 <dhruv2k19.3000@gmail.com>
2024-08-07 19:07:48 +0530

    Initial commit

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+    console.log("Hero component is rendering")
+  return (
+    <div className='hero'>
+      
+    </div>
+  );
+}
+
+export default Hero
